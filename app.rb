9e2a1b0176982e91c0a6ca9af11695a4cecb3ba9
anupama <anupamagadde@yahoo.com>
2020-01-27 14:40:45 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Red Lion Fluffington"
+    end
+
+    get '/hometown' do
+        "My hometown is Magical Jungle"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Father Brown"
+    end
 end
