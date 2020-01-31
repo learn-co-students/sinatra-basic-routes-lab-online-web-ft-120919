83f7f094937c8c7b26edea20b7f3f77743a31ea7
nwscala <nwscala@gmail.com>
2020-01-31 14:03:25 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get "/name" do
+        "My name is Noah."
+    end
+
+    get "/hometown" do
+        "My hometown is Glen Rock, NJ."
+    end
+
+    get "/favorite-song" do
+        "My favorite song is Blue by The Seatbelts."
+    end
 end
