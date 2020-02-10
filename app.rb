a4f821afc16c2089f95e92cc3b8ea304ea9e4465
William Stevens <wstevens.hornet@gmail.com>
2020-02-10 09:16:03 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is William"
+  end
+
+  get '/hometown' do
+    "My hometown is Burlington"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Nujabes"
+  end
+
+
 end
