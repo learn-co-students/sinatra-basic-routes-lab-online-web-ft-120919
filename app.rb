f74d2527b56d941e8cd347894d6a69d02fb473d1
Nat McConnaughay <natmcconnaughay@gmail.com>
2020-02-19 02:18:43 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Nat"
+    end
+
+    get '/hometown' do
+        "My hometown is Peoria"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Old Town Road"
+    end
+
 end
